43dd6ae90477dc4524b12346509d39f06d7f59cd
Praveen <bankhedepraveen12@gmail.com>
2024-09-26 21:23:37 +0530

    update coaching name

--- client/screens/onboarding/onboarding.screen.tsx
+++ client/screens/onboarding/onboarding.screen.tsx
@@ -45,7 +45,7 @@ export default function OnBoardingScreen() {
             source={require("@/assets/onboarding/shape_6.png")}
           />
           <Text style={[styles.titleText, { fontFamily: "Raleway_700Bold" }]}>
-            Becodemy
+            Entrance Exam Warriors
           </Text>
         </View>
         <View style={styles.dscpWrapper}>
