8c552ed16ee5d7a109f6a6e54f6471121ebd766d
Praveen <bankhedepraveen12@gmail.com>
2024-09-27 16:31:52 +0530

    .

--- client/utils/uri.tsx
+++ client/utils/uri.tsx
@@ -1,6 +1,6 @@
 // export const SERVER_URI = "http://localhost:8000/api/v1";
-export const SERVER_URI = "http://192.168.1.113:8000/api/v1";
+// export const SERVER_URI = "http://192.168.1.113:8000/api/v1";
 // export const SERVER_URI = "http://192.168.102.41:8000/api/v1";
-// export const SERVER_URI = "https://lms-bupn.onrender.com/api/v1";
+export const SERVER_URI = "https://lms-bupn.onrender.com/api/v1";
 // export const SERVER_URI = "https://lms-bupn.onrender.com/";
 // https://lms-bupn.onrender.com
